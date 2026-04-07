a269fe56ad746cf25e38276782ac5e72452c2ff7
ldTruong04 <ledinhtruong2004@gmail.com>
2026-04-07 22:05:16 +0700

    update tuan4 cuoi

---
+++ Buoi4_Docker/bai3/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "24px" }}>
+      <h1>Bai 3 - React in Docker</h1>
+      <p>Lê Đình Trường</p>
+    </div>
+  );
+}
+
+export default App;
